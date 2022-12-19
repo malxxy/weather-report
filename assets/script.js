524b7786c4dbaadee4a2b78c821063c152279130
malxxy <maliayraguen@gmail.com>
2022-12-18 22:31:50 -0800

    styling home page

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,27 @@
-// Variable to wrap everything in jQuery
+$(document).ready(function() { 
+    var searchBtn = document.getElementById("search-btn");
 
-// // Store city
-// $("#city").setItem.
-// // Get latitude and longitude from city using open maps API
+    // Store city
+    var city = document.getitem
+    lat = 12.890
+    lon = -135.20
+    // $("#city").setItem.
+    // Get latitude and longitude from city using open maps API
 
-var weatherApiKey = "78785b54a90c1a313e4af8f23972a484"; // key #1
-var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={${weatherApiKey}}`;
-var weatherUrl2 = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={073286d3d6d5eda4c8c7cb9e0c3c94d5}"; // manually typed in key #2
-// fetch open Weather API
-fetch(weatherUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('Fetch Response \n-------------');
-    console.log(data);
-  });
+    var weatherApiKey = "78785b54a90c1a313e4af8f23972a484"; // key #1
+    var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
+    
+    // fetch open Weather API
+    function getWeather() {
+        fetch(weatherUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log('Fetch Response \n-------------');
+            console.log(data);
+        });
+    };
+
+    $("searchBtn").on("click",getWeather())
+});
